17ec75e11aa2ce423d4f5c27634a1ee8fd0c3027
Felipe Andrade <araujoandrade.flp@gmail.com>
2016-07-12 20:47:57 -0300

    refactoring exception

--- src/es2/exercicio2/Funcionario.java
+++ src/es2/exercicio2/Funcionario.java
@@ -4,55 +4,55 @@ import java.math.BigDecimal;
 
 public class Funcionario {
 
-	private BigDecimal salarioBase;
-	private Cargo cargo;
-	private String nome;
-	private String email;
+    private BigDecimal salarioBase;
+    private Cargo cargo;
+    private String nome;
+    private String email;
 
-	public Funcionario(String nome, String email, double salarioBase) throws Exception {
-		if (nome == null || nome.trim().equals("")) {
-			throw new Exception("Nome de usuario nao pode ser vazio ou null.");
-		}
-		if (salarioBase <= 0.0) {
-			throw new Exception("Salario base nao pode ser negativo ou zero.");
-		}
-		if (email == null || email.trim().equals("")) {
-			throw new Exception("Email nao pode ser null ou vazio.");
-		}
-		if (!email.matches("^.+@.+(\\.[^\\.]+)+$")) {
-			throw new Exception("Email invalido.");
-		}
-		this.nome = nome;
-		this.email = email;
-		this.salarioBase = new BigDecimal(salarioBase);
-		this.cargo = new Desenvolvedor();
+    public Funcionario(String nome, String email, double salarioBase) throws IllegalArgumentException {
+	if (nome == null || nome.trim().equals("")) {
+	    throw new IllegalArgumentException("Nome de usuario nao pode ser vazio ou null.");
 	}
+	if (salarioBase <= 0.0) {
+	    throw new IllegalArgumentException("Salario base nao pode ser negativo ou zero.");
+	}
+	if (email == null || email.trim().equals("")) {
+	    throw new IllegalArgumentException("Email nao pode ser null ou vazio.");
+	}
+	if (!email.matches("^.+@.+(\\.[^\\.]+)+$")) {
+	    throw new IllegalArgumentException("Email invalido.");
+	}
+	this.nome = nome;
+	this.email = email;
+	this.salarioBase = new BigDecimal(salarioBase);
+	this.cargo = new Desenvolvedor();
+    }
 
-	public double calculaSalario() {
-		return cargo.calculaSalario(salarioBase).doubleValue();
-	}
+    public double calculaSalario() {
+	return cargo.calculaSalario(salarioBase).doubleValue();
+    }
 
-	public void setSalario(double novoSalario) throws Exception {
-		if (novoSalario <= 0.0) {
-			throw new Exception("Salario base nao pode ser negativo ou zero.");
-		}
-		salarioBase = new BigDecimal(novoSalario);
+    public void setSalario(double novoSalario) throws IllegalArgumentException {
+	if (novoSalario <= 0.0) {
+	    throw new IllegalArgumentException("Salario base nao pode ser negativo ou zero.");
 	}
+	salarioBase = new BigDecimal(novoSalario);
+    }
 
-	public void setCargo(Cargo cargo) {
-		this.cargo = cargo;
-	}
+    public void setCargo(Cargo cargo) {
+	this.cargo = cargo;
+    }
 
-	public String getNome() {
-		return nome;
-	}
+    public String getNome() {
+	return nome;
+    }
 
-	public String getEmail() {
-		return email;
-	}
+    public String getEmail() {
+	return email;
+    }
 
-	public double getSalarioBase() {
-		return salarioBase.doubleValue();
-	}
+    public double getSalarioBase() {
+	return salarioBase.doubleValue();
+    }
 
 }
--- test/es2/exercicio2/FuncionarioTest.java
+++ test/es2/exercicio2/FuncionarioTest.java
@@ -6,151 +6,151 @@ import org.junit.Test;
 
 public class FuncionarioTest {
 
-	private Funcionario funcionario;
+    private Funcionario funcionario;
 
-	@Before
-	public void setUp() throws Exception {
-		funcionario = new Funcionario("Juliao Petruchio", "jpetruchio@gmail.com", 3000.00);
+    @Before
+    public void setUp() {
+	funcionario = new Funcionario("Juliao Petruchio", "jpetruchio@gmail.com", 3000.00);
+    }
+
+    @Test
+    public void testFuncionario() {
+	Assert.assertEquals("Juliao Petruchio", funcionario.getNome());
+	Assert.assertEquals("jpetruchio@gmail.com", funcionario.getEmail());
+	Assert.assertEquals(3000.00, funcionario.getSalarioBase(), 0.0);
+    }
+
+    @Test
+    public void testFuncionarioNomeInvalido() {
+
+	try {
+	    new Funcionario(null, "jpetruchio@gmail.com", 1888.00);
+	    Assert.fail("Nao deveria passar.");
+	} catch (IllegalArgumentException e) {
+	    Assert.assertEquals("Nome de usuario nao pode ser vazio ou null.", e.getMessage());
 	}
 
-	@Test
-	public void testFuncionario() {
-		Assert.assertEquals("Juliao Petruchio", funcionario.getNome());
-		Assert.assertEquals("jpetruchio@gmail.com", funcionario.getEmail());
-		Assert.assertEquals(3000.00, funcionario.getSalarioBase(), 0.0);
+	try {
+	    new Funcionario(" ", "jpetruchio@gmail.com", 1888.00);
+	    Assert.fail("Nao deveria passar.");
+	} catch (IllegalArgumentException e) {
+	    Assert.assertEquals("Nome de usuario nao pode ser vazio ou null.", e.getMessage());
+	}
+    }
+
+    @Test
+    public void testFuncionarioEmailInvalido() {
+
+	try {
+	    new Funcionario("Juliao Petruchio", "@gmail.com", 2000.00);
+	    Assert.fail("Nao deveria passar.");
+	} catch (IllegalArgumentException e) {
+	    Assert.assertEquals("Email invalido.", e.getMessage());
 	}
 
-	@Test
-	public void testFuncionarioNomeInvalido() {
-
-		try {
-			new Funcionario(null, "jpetruchio@gmail.com", 1888.00);
-			Assert.fail("Nao deveria passar.");
-		} catch (Exception e) {
-			Assert.assertEquals("Nome de usuario nao pode ser vazio ou null.", e.getMessage());
-		}
-
-		try {
-			new Funcionario(" ", "jpetruchio@gmail.com", 1888.00);
-			Assert.fail("Nao deveria passar.");
-		} catch (Exception e) {
-			Assert.assertEquals("Nome de usuario nao pode ser vazio ou null.", e.getMessage());
-		}
+	try {
+	    new Funcionario("Juliao Petruchio", "jpetruchio@", 2000.00);
+	    Assert.fail("Nao deveria passar.");
+	} catch (IllegalArgumentException e) {
+	    Assert.assertEquals("Email invalido.", e.getMessage());
 	}
 
-	@Test
-	public void testFuncionarioEmailInvalido() {
-
-		try {
-			new Funcionario("Juliao Petruchio", "@gmail.com", 2000.00);
-			Assert.fail("Nao deveria passar.");
-		} catch (Exception e) {
-			Assert.assertEquals("Email invalido.", e.getMessage());
-		}
-
-		try {
-			new Funcionario("Juliao Petruchio", "jpetruchio@", 2000.00);
-			Assert.fail("Nao deveria passar.");
-		} catch (Exception e) {
-			Assert.assertEquals("Email invalido.", e.getMessage());
-		}
-
-		try {
-			new Funcionario("Juliao Petruchio", " ", 2000.00);
-			Assert.fail("Nao deveria passar.");
-		} catch (Exception e) {
-			Assert.assertEquals("Email nao pode ser null ou vazio.", e.getMessage());
-		}
-
-		try {
-			new Funcionario("Juliao Petruchio", null, 2000.00);
-			Assert.fail("Nao deveria passar.");
-		} catch (Exception e) {
-			Assert.assertEquals("Email nao pode ser null ou vazio.", e.getMessage());
-		}
+	try {
+	    new Funcionario("Juliao Petruchio", " ", 2000.00);
+	    Assert.fail("Nao deveria passar.");
+	} catch (IllegalArgumentException e) {
+	    Assert.assertEquals("Email nao pode ser null ou vazio.", e.getMessage());
 	}
 
-	@Test
-	public void testFuncionarioSalarioBaseInvalido() {
+	try {
+	    new Funcionario("Juliao Petruchio", null, 2000.00);
+	    Assert.fail("Nao deveria passar.");
+	} catch (IllegalArgumentException e) {
+	    Assert.assertEquals("Email nao pode ser null ou vazio.", e.getMessage());
+	}
+    }
 
-		try {
-			funcionario.setSalario(0);
-			Assert.fail("Nao deveria passar.");
-		} catch (Exception e) {
-			Assert.assertEquals("Salario base nao pode ser negativo ou zero.", e.getMessage());
-		}
+    @Test
+    public void testFuncionarioSalarioBaseInvalido() {
 
-		try {
-			funcionario.setSalario(-900);
-			Assert.fail("Nao deveria passar.");
-		} catch (Exception e) {
-			Assert.assertEquals("Salario base nao pode ser negativo ou zero.", e.getMessage());
-		}
-
-		try {
-			new Funcionario("Juliao Petruchio", "jpetruchio@gmail.com", 0.00);
-			Assert.fail("Nao deveria passar.");
-		} catch (Exception e) {
-			Assert.assertEquals("Salario base nao pode ser negativo ou zero.", e.getMessage());
-		}
-
-		try {
-			new Funcionario("Juliao Petruchio", "jpetruchio@gmail.com", -100.00);
-			Assert.fail("Nao deveria passar.");
-		} catch (Exception e) {
-			Assert.assertEquals("Salario base nao pode ser negativo ou zero.", e.getMessage());
-		}
+	try {
+	    funcionario.setSalario(0);
+	    Assert.fail("Nao deveria passar.");
+	} catch (IllegalArgumentException e) {
+	    Assert.assertEquals("Salario base nao pode ser negativo ou zero.", e.getMessage());
 	}
 
-	@Test
-	public void testSalarioDesenvolvedor3MilOuMais() {
-		Assert.assertEquals(2400.00, funcionario.calculaSalario(), 0.0);
+	try {
+	    funcionario.setSalario(-900);
+	    Assert.fail("Nao deveria passar.");
+	} catch (IllegalArgumentException e) {
+	    Assert.assertEquals("Salario base nao pode ser negativo ou zero.", e.getMessage());
 	}
 
-	@Test
-	public void testSalarioDesenvolvedorMenorQue3Mil() throws Exception {
-		funcionario.setSalario(2800.00);
-		Assert.assertEquals(2520.00, funcionario.calculaSalario(), 0.0);
+	try {
+	    new Funcionario("Juliao Petruchio", "jpetruchio@gmail.com", 0.00);
+	    Assert.fail("Nao deveria passar.");
+	} catch (IllegalArgumentException e) {
+	    Assert.assertEquals("Salario base nao pode ser negativo ou zero.", e.getMessage());
 	}
 
-	@Test
-	public void testSalarioDBA2MilOuMais() throws Exception {
-		funcionario.setCargo(new DBA());
-		funcionario.setSalario(2000.00);
-		Assert.assertEquals(1500.00, funcionario.calculaSalario(), 0.0);
+	try {
+	    new Funcionario("Juliao Petruchio", "jpetruchio@gmail.com", -100.00);
+	    Assert.fail("Nao deveria passar.");
+	} catch (IllegalArgumentException e) {
+	    Assert.assertEquals("Salario base nao pode ser negativo ou zero.", e.getMessage());
 	}
+    }
 
-	@Test
-	public void testSalarioDBAMenorQue2Mil() throws Exception {
-		funcionario.setCargo(new DBA());
-		funcionario.setSalario(1999.00);
-		Assert.assertEquals(1699.15, funcionario.calculaSalario(), 0.001);
-	}
+    @Test
+    public void testSalarioDesenvolvedor3MilOuMais() {
+	Assert.assertEquals(2400.00, funcionario.calculaSalario(), 0.0);
+    }
 
-	@Test
-	public void testSalarioTestador2MilOuMais() {
-		funcionario.setCargo(new Testador());
-		Assert.assertEquals(2250, funcionario.calculaSalario(), 0.0);
-	}
+    @Test
+    public void testSalarioDesenvolvedorMenorQue3Mil() {
+	funcionario.setSalario(2800.00);
+	Assert.assertEquals(2520.00, funcionario.calculaSalario(), 0.0);
+    }
 
-	@Test
-	public void testSalarioTestadorMenorQue2Mil() throws Exception {
-		funcionario.setCargo(new Testador());
-		funcionario.setSalario(1900.00);
-		Assert.assertEquals(1615.00, funcionario.calculaSalario(), 0.0);
-	}
+    @Test
+    public void testSalarioDBA2MilOuMais() {
+	funcionario.setCargo(new DBA());
+	funcionario.setSalario(2000.00);
+	Assert.assertEquals(1500.00, funcionario.calculaSalario(), 0.0);
+    }
 
-	@Test
-	public void testSalarioGerente5MilOuMais() throws Exception {
-		funcionario.setCargo(new Gerente());
-		funcionario.setSalario(5000.00);
-		Assert.assertEquals(3500.00, funcionario.calculaSalario(), 0.0);
-	}
+    @Test
+    public void testSalarioDBAMenorQue2Mil() {
+	funcionario.setCargo(new DBA());
+	funcionario.setSalario(1999.00);
+	Assert.assertEquals(1699.15, funcionario.calculaSalario(), 0.001);
+    }
 
-	@Test
-	public void testSalarioGerenteMenorQue5Mil() {
-		funcionario.setCargo(new Gerente());
-		Assert.assertEquals(2400.00, funcionario.calculaSalario(), 0.0);
-	}
+    @Test
+    public void testSalarioTestador2MilOuMais() {
+	funcionario.setCargo(new Testador());
+	Assert.assertEquals(2250, funcionario.calculaSalario(), 0.0);
+    }
+
+    @Test
+    public void testSalarioTestadorMenorQue2Mil() {
+	funcionario.setCargo(new Testador());
+	funcionario.setSalario(1900.00);
+	Assert.assertEquals(1615.00, funcionario.calculaSalario(), 0.0);
+    }
+
+    @Test
+    public void testSalarioGerente5MilOuMais() {
+	funcionario.setCargo(new Gerente());
+	funcionario.setSalario(5000.00);
+	Assert.assertEquals(3500.00, funcionario.calculaSalario(), 0.0);
+    }
+
+    @Test
+    public void testSalarioGerenteMenorQue5Mil() {
+	funcionario.setCargo(new Gerente());
+	Assert.assertEquals(2400.00, funcionario.calculaSalario(), 0.0);
+    }
 
 }
